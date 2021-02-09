8fd312c5251c152ffd33dc3eefd88c4457ad3f12
Andrew Barron <awmb1@st-andrews.ac.uk>
2021-02-09 15:23:50 +0000

    Implementation of `removeInv`

--- src/Actions.hs
+++ src/Actions.hs
@@ -88,7 +88,12 @@ addInv gd obj = undefined
    the inventory. -}
 
 removeInv :: GameData -> String -> GameData
-removeInv gd obj = undefined
+removeInv gd obj = GameData (location_id gd)
+                            (world gd)
+                            (filter (\candidate -> not(obj_name candidate == obj)) (inventory gd))
+                            (poured gd)
+                            (caffeinated gd)
+                            (finished gd)
 
 {- Does the inventory in the game state contain the given object? -}
 
