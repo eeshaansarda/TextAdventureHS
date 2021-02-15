c4c923cd35fff3c51a1a0b8066b54901706d1aba
Andrew Barron <awmb1@st-andrews.ac.uk>
2021-02-15 15:53:31 +0000

    Add Haddock comments to Adventure.hs

--- src/Adventure.hs
+++ src/Adventure.hs
@@ -12,33 +12,26 @@ import Data.Char
 
 winmessage = "Congratulations, you have made it outside.\n"
 
-{- Given a game state, and user input (as a list of words) return a 
-   new game state and a message for the user. -}
-
-{-
-process :: GameData -> [String] -> (GameData, String)
-process state [cmd,arg] = case actions cmd of
-                            Just fn -> fn arg state
-                            Nothing -> (state, "I don't understand")
-process state [cmd]     = case commands cmd of
-                            Just fn -> fn state
-                            Nothing -> (state, "I don't understand")
-process state _ = (state, "I don't understand")
--}
-
+-- TODO Report: this isn't completely random since Haskell likes pure functions and randomness breaks purity
+{-| For a given character, return either the character or a fuzzy nonsense character.
+    A given character will always return the same result. Punctuation will never be fuzzy. -}
 fuzz :: Char -> Char
 fuzz x | not (isAlphaNum x) = x
        | even (ord x)       = '▒'
        | otherwise          = x
 
+{-| Calls putStr with the provided String. If the GameData shows that the player is
+       currently blind, the characters are fuzzed using `fuzz` first. -}
 putStrFuzzy :: GameData -> String -> IO ()
 putStrFuzzy state string | blind state = putStr (map (fuzz) string)
                          | otherwise   = putStr string
                          
+{- | Calls putStrFuzzy after appending a line break to the string. -}
 putStrLnFuzzy :: GameData -> String -> IO ()
 putStrLnFuzzy state string = putStrFuzzy state (string ++ "\n")
 
-
+{- | Main game loop: returning a response to an action, prompting for a command, 
+     parsing input, then executing the command. -}
 repl :: GameData -> IO GameData
 repl state | finished state = return state
 repl state = do putStrFuzzy state (show state)
@@ -51,6 +44,7 @@ repl state = do putStrFuzzy state (show state)
                                         return state'
                                else repl state'
 
+{-| Starts the main game loop. -}
 main :: IO ()
 main = do repl initState
           return ()
